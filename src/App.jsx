e3e178d0d20c596a6e96c2a51225a7ca37cb1956
EA - Natalia Rodrigues Costa <costa_natalia@educadventista.org>
2025-05-20 13:34:03 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,44 @@
-import {useState} from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
+export function App() {
+  const [numero, setNumero] = useState(100);
+  const [nome, setNome] = useState("Natalia");
+  const [mudouNome, setMudouNome] = useState(false);
 
-function App(){
-  const [contador, setContador] = useState (100);
-  const [mostrar, setMostrar] = useState(true);
-
-  return(
-    <div>
-      <h1>Contador :{contador}</h1>
-      <button onClick = {()=> setContador(contador +100)}>Acrescentar</button>
-      
-      
-      <br></br>
-
-      <button onClick = {() => setMostrar(!mostrar)}> 
-          {mostrar ? 'Esconder' : 'Mostrar'}
-      </button>
-      {mostrar ? <p>Sou a Natália</p> : null}
-           
-    </div>
-  );
+  function handleAumentar() {
+    setNumero(numero + 100);
   }
-export default App;
+
+  function handleDiminuir() {
+    setNumero(numero - 100);
+  }
+
+  function handleAlterarNome() {
+    if (mudouNome === false) {
+      setMudouNome(true);
+      setNome("Nati");
+      return;
+    }
+
+    setMudouNome(false);
+    setNome("Natalia");
+  }
+
+  return (
+    <main className="container">
+      <h1 className="titulo">App</h1>
+
+      <section>
+        <p>O número da variável é: {numero}</p>
+        <button onClick={handleAumentar}>Aumentar</button>
+        <button onClick={handleDiminuir}>Diminuir</button>
+      </section>
+
+      <section>
+        <p>O nome é: {nome}</p>
+        <button onClick={handleAlterarNome}>Alterar nome</button>
+      </section>
+    </main>
+  );
+}
